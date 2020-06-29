a2e4b6d845e6fea6c0e3113af781b0abdd5e83c6
yuziem14 <yuri.fnunes@gmail.com>
2020-06-29 20:15:09 -0300

    feat: config routes
    Co-authored-by: Anderson Alex Durante <anderson.alexdurante@gmail.com>

---
+++ server/start/routes.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.get('/', () => {
+  return { greeting: 'LeiturÁvore API' }
+})
+
+Route.post('register', 'Auth/RegisterController.store')
+Route.post('signin', 'Auth/SessionController.store')
+Route.post('refresh', 'Auth/SessionController.update')
+Route.get('user/categories', 'UserCategoryController.index').middleware([
+  'auth',
+])
+
+Route.get('categories', 'CategoryController.index')
