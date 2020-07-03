bab214d2d3fbea9e6e9ccf271721c789f0f561b3
yuziem14 <yuri.fnunes@gmail.com>
2020-07-03 17:49:07 -0300

    chore: add logo to assets and global styles
    Co-authored-by: Anderson Alex Durante <anderson.alexdurante@gmail.com>

---
+++ mobile/src/globalStyles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import Constants from 'expo-constants';
+
+export const GlobalContainer = styled.View`
+  flex: 1;
+  background-color: #220066;
+  padding-top: ${Constants.statusBarHeight}px;
+`;
